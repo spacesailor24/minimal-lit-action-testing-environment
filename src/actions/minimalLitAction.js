d16aa800fc42b4c3408b26b1d5e2bfbcb6be0469
Wyatt Barnes <me@wyatt.email>
2024-07-03 18:09:44 -1000

    Fix to get test passing

--- src/actions/minimalLitAction.js
+++ src/actions/minimalLitAction.js
@@ -1,13 +1,13 @@
 const go = async () => {
-    // this is the string "Hello World" for testing
-    const toSign = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100];
-    // this requests a signature share from the Lit Node
-    // the signature share will be automatically returned in the HTTP response from the node
-    const sigShare = await LitActions.signEcdsa({
-        toSign,
-        publicKey, // <-- You should pass this in jsParam
-        sigName: "sig1",
-    });
+  // this is the string "Hello World" for testing
+  const toSign = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100];
+  // this requests a signature share from the Lit Node
+  // the signature share will be automatically returned in the HTTP response from the node
+  const sigShare = await LitActions.signEcdsa({
+    toSign: ethers.utils.arrayify(ethers.utils.keccak256(toSign)),
+    publicKey, // <-- You should pass this in jsParam
+    sigName: "sig1",
+  });
 };
 
 go();
--- src/getWalletSessionSigs.ts
+++ src/getWalletSessionSigs.ts
@@ -1,20 +1,29 @@
 //NEW METHOD DO GET SESSION SIGS
 import { ethers } from "ethers";
 import { SiweMessage } from "siwe";
-import { LitNodeClient } from '@lit-protocol/lit-node-client';
-import { LitAbility } from '@lit-protocol/types';
-import { LitAccessControlConditionResource, LitActionResource, newSessionCapabilityObject } from "@lit-protocol/auth-helpers";
-import {LIT_NETWORKS_KEYS} from '@lit-protocol/types';
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+import { LitAbility } from "@lit-protocol/types";
+import {
+  LitAccessControlConditionResource,
+  LitActionResource,
+  LitPKPResource,
+  createSiweMessageWithRecaps,
+  generateAuthSig,
+  newSessionCapabilityObject,
+} from "@lit-protocol/auth-helpers";
+import { LIT_NETWORKS_KEYS } from "@lit-protocol/types";
 interface AuthSigProp {
   privateKey: string;
   chainId: number;
   litActionCid: string;
 }
 
-export const getWalletSessionSigs = async (prop: AuthSigProp, litNetwork: LIT_NETWORKS_KEYS) => {
+export const getWalletSessionSigs = async (
+  prop: AuthSigProp,
+  litNetwork: LIT_NETWORKS_KEYS
+) => {
   let { privateKey, chainId } = prop;
   const wallet = new ethers.Wallet(privateKey);
-
 
   const litNodeClient = new LitNodeClient({
     litNetwork: litNetwork,
@@ -25,31 +34,24 @@ export const getWalletSessionSigs = async (prop: AuthSigProp, litNetwork: LIT_NE
 
   const nonce = await litNodeClient.getLatestBlockhash();
 
-  const authNeededCallback = async ({ resources, expiration: expirationTime, uri :origin }) => {
-
-    const message = new SiweMessage({
-      domain: 'node',
-      address: wallet.address,
-      uri: origin,
-      statement: `Sign a session key to use with Lit Protocol`,
-      version: '1',
-      chainId: chainId,
-      expirationTime,
-      resources,
+  const authNeededCallback = async ({
+    resourceAbilityRequests,
+    expiration,
+    uri,
+  }) => {
+    const toSign = await createSiweMessageWithRecaps({
+      uri,
+      expiration,
+      resources: resourceAbilityRequests,
+      walletAddress: wallet.address,
       nonce,
+      litNodeClient,
     });
 
-    const toSign = message.prepareMessage();
-    const signature = await wallet.signMessage(toSign);
-
-    const authSig = {
-      sig: signature,
-      derivedVia: "web3.eth.personal.sign",
-      signedMessage: toSign,
-      address: wallet.address,
-    };
-
-    return authSig;
+    return await generateAuthSig({
+      signer: wallet,
+      toSign,
+    });
   };
 
   // @todo: need to check whether the LitAbility.PKPSigning capability is the best one for scoping
@@ -59,9 +61,13 @@ export const getWalletSessionSigs = async (prop: AuthSigProp, litNetwork: LIT_NE
     expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(), // 24 hours
     resourceAbilityRequests: [
       {
-        resource: new LitActionResource('*'),
+        resource: new LitPKPResource("*"),
+        ability: LitAbility.PKPSigning,
+      },
+      {
+        resource: new LitActionResource("*"),
         ability: LitAbility.LitActionExecution,
-      }
+      },
     ],
     // @ts-ignore
     authNeededCallback,
